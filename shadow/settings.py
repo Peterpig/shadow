cfa6b56147ae0df7a8c9001873dd09d4bdc6ee95
zhoupeilin <309871271@qq.com>
2017-12-06 13:35:23 +0800

    降低请求频率 防止被封

--- shadow/settings.py
+++ shadow/settings.py
@@ -22,14 +22,14 @@ NEWSPIDER_MODULE = 'shadow.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 10
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 4
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 10
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
